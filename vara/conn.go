f30e7e79f6d408ab6420758414c2075cc6cd4d76
Chris Keller <xylo04@gmail.com>
2021-10-15 10:54:32 -0600

    Changes based on experience with VARA-FM testing
    * varaDataConn.LocalAddr and RemoteAddr should be callsigns, not data TCP port
    * Set MYCALL
    * Don't set bandwidth 2300 for VARA-FM
    * Refactor out Modem.handleCmd for readability

--- vara/conn.go
+++ vara/conn.go
@@ -4,7 +4,7 @@ import (
 	"net"
 )
 
-// Wrapper for the data port connection we hand to clients.
+// Wrapper for the data port connection we hand to clients. Implements net.Conn.
 type varaDataConn struct {
 	// the underlying TCP conn we're wrapping (type embedding)
 	net.TCPConn
@@ -14,7 +14,23 @@ type varaDataConn struct {
 
 // Close closes the connection.
 // Any blocked Read or Write operations will be unblocked and return errors.
+//
+// "Overrides" net.Conn.Close.
 func (v *varaDataConn) Close() error {
 	// If client wants to close the data stream, close down RF and TCP as well
 	return v.modem.Close()
+}
+
+// LocalAddr returns the local network address.
+//
+// "Overrides" net.Conn.LocalAddr.
+func (v *varaDataConn) LocalAddr() net.Addr {
+	return Addr{v.modem.myCall}
+}
+
+// RemoteAddr returns the remote network address.
+//
+// "Overrides" net.Conn.RemoteAddr.
+func (v *varaDataConn) RemoteAddr() net.Addr {
+	return Addr{v.modem.toCall}
 }
